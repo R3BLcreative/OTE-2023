c1afaf9fd7fb94878eaf1c05bce65cc8fd5e09c9
James Cook <ha-jcook@James-Cook.local>
2022-09-26 13:25:16 -0500

    Updated with dates and special guests

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -35,6 +35,7 @@
 
 	<link rel="preconnect" href="https://fonts.googleapis.com">
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link rel="preconnect" href="https://kit.fontawesome.com">
 
 	<link href="https://fonts.googleapis.com/css2?family=Bangers&family=Bitter:ital,wght@0,400;0,500;0,600;0,700;0,800;0,900;1,400;1,500;1,600;1,700;1,800;1,900&family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap" rel="stylesheet">
 
@@ -47,6 +48,7 @@
 
 	<!-- Scripts & Styles -->
 	@vite(['resources/css/app.css', 'resources/js/app.js'])
+	<script src="https://kit.fontawesome.com/743b4f4183.js" crossorigin="anonymous"></script>
 </head>
 
 <body {{ $attributes->merge(['class' => 'font-sans font-normal text-body-100 mobile:text-md laptop:text-base tracking-wide antialiased bg-black bg-stage bg-no-repeat bg-center bg-fixed bg-cover']) }}>
