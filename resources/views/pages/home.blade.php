9e900b1c5b29d85ce054bc5ec32cf23ca160f7d7
James Cook <ha-jcook@James-Cook.local>
2022-12-06 13:22:04 -0600

    Node Updates

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,11 +1,4 @@
-<x-layouts::default
-	:seo="[
-	'metaTitle' => 'Over the Edge Preteen Camp || Camp 2023',
-	'metaDescription' => 'OTE Camp is a 5 day faith based Christian summer camp for students who have completed 3rd - 6th grade. Hosted at Texas Baptist Encampment in Palacious, TX.',
-	'preventIndexing' => false
-	]">
 
-	<x-slot name="main">
 
 		{{-- HERO --}}
 		<x-components::hero class="bg-ote bg-cover bg-center bg-no-repeat" row="items-center">
