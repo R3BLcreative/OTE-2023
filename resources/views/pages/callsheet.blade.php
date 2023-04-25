4e4e5ac38e02fa2074daea9f3d416324606106ff
James Cook <james.cook@helloalice.com>
2023-04-25 16:59:36 -0500

    Started dev of callsheet functionality

--- resources/views/pages/callsheet.blade.php
+++ resources/views/pages/callsheet.blade.php
@@ -12,7 +12,9 @@
 					<h1 class="h1 text-secondary-100 text-center mb-4">
 						OTE Prize Callsheet
 					</h1>
-					<p class="text-white text-center">To generate a callsheet, simply fill out the form below and click submit.<br>The form will be replaced by a list of randomly selected names.<br>As you call the names, click/tap the name to remove it from the list.</p>
+					<p class="text-white text-center">To generate a callsheet, simply fill out the form below and click submit.</p>
+					<p class="text-white text-center">The form will be replaced by a list of randomly selected names.</p>
+					<p class="text-white text-center">As you call the names, click/tap the name to remove it from the list.</p>
 				</div>
 			</row>
 			<row class="!py-0">
